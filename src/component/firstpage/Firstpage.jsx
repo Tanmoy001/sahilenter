922458b88e0063bba8686c6aea7c77ad589a5edf
Tanmoy Chowdhury <chowdhurytanmoy580.com>
2024-03-13 02:22:16 +0530

    processing

--- src/component/firstpage/Firstpage.jsx
+++ src/component/firstpage/Firstpage.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Home from './home/Home'
 import  Aboutcom from './aboutcom/Aboutcom';
 import Home_service from './home_service/Home_service';
+import Chooseus from './chooseus/Chooseus';
+import Contact from './home_contanct/Contact';
 function Firstpage() {
   return (
     <div>
       <Home/>
       <Aboutcom/>
       <Home_service/>
+      <Chooseus/>
+      <Contact/>
     </div>
   )
 }
---
+++ src/component/firstpage/home_contanct/Contact.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './contacts.css';
+function Contact() {
+    return (
+        <div className='home_contactus_section'>
+            <div className='contectus_container'>
+                <div className='backgroung_pic'>
+                    {/* <img src={pic} alt='profile-picture' /> */}
+
+                   
+                        <div className='info_container'>
+                            <div className='heading_section'>
+                                <h5>With over 15 years of experience in the industry</h5>
+                            <h3>Best Engineering & Construction Company In India</h3>
+                            </div>
+
+                            <div className='content_section'>
+                                <div className='content_section_one'>
+                                <h5>Address</h5>
+                                <h4>Our Office</h4>
+                                <p>Khalpol , Thakurpukur, Kolkata - 700063</p>
+                                <div className='phone_number'><p>Phone: +91 033-4005-2352</p></div>
+                                <div className='email'>Email: info@bestengineering.</div>
+                                </div>
+                                <div className='Contact_section'>
+                                    <p>sadkfhasdjkdfhasdbgfkjasldhfddlk</p>
+                                 </div>
+                            </div>
+                            
+                        </div>
+                   
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Contact
