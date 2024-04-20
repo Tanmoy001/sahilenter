deab8a9fa5a340f15ce83bc51642f14139bfe370
Tanmoy Chowdhury <chowdhurytanmoy580.com>
2024-04-20 15:09:17 +0530

    ChooseUs

---
+++ src/component/firstpage/chooseus/ChooseUs.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import pic from './bkg.png';
+import'./chooseus.css';
+import { FaAward } from "react-icons/fa";
+function ChooseUs() {
+  return (
+    <div className='Cooseus_section'>
+      <div className='cooseus_container'>
+        <img src={pic} alt="background" />
+        <div className='Content_section'>
+          <h4>Why Choose Us</h4>
+          <h2>Six Reasons For People Choosing  Our Services.</h2>
+          <div className='reasons_sections'>
+            <div className='reasons_content'>
+              <FaAward />
+              <h5>Proven Expertise</h5>
+              <p>With over 15 years of experience in the industry, ESSDEE PROJECTS has a track record of successfully delivering a wide range of civil and mechanical construction projects.</p>
+            </div>
+            <div className='reasons_content'>
+              <FaAward />
+              <h5>Proven Expertise</h5>
+              <p>With over 15 years of experience in the industry, ESSDEE PROJECTS has a track record of successfully delivering a wide range of civil and mechanical construction projects.</p>
+            </div>
+            <div className='reasons_content'>
+              <FaAward />
+              <h5>Proven Expertise</h5>
+              <p>With over 15 years of experience in the industry, ESSDEE PROJECTS has a track record of successfully delivering a wide range of civil and mechanical construction projects.</p>
+            </div>
+            <div className='reasons_content'>
+              <FaAward />
+              <h5>Proven Expertise</h5>
+              <p>With over 15 years of experience in the industry, ESSDEE PROJECTS has a track record of successfully delivering a wide range of civil and mechanical construction projects.</p>
+            </div>
+            <div className='reasons_content'>
+              <FaAward />
+              <h5>Proven Expertise</h5>
+              <p>With over 15 years of experience in the industry, ESSDEE PROJECTS has a track record of successfully delivering a wide range of civil and mechanical construction projects.</p>
+            </div>
+            <div className='reasons_content'>
+              <FaAward />
+              <h5>Proven Expertise</h5>
+              <p>With over 15 years of experience in the industry, ESSDEE PROJECTS has a track record of successfully delivering a wide range of civil and mechanical construction projects.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default ChooseUs
